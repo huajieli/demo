8407c8be5fe28be4bd72af99d4457f1f20ff9892
lihuajie <lihuajie@LAPTOP-V2SDSN7A.lan>
2019-03-19 22:56:11 +0800

    jxl读取excel信息（太简单了实用性不强，只能帮助理解）

--- src/main/java/com/daily/excel/TestJxl.java
+++ src/main/java/com/daily/excel/TestJxl.java
@@ -1,10 +1,14 @@
 package com.daily.excel;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
 
+import jxl.Cell;
+import jxl.Sheet;
 import jxl.Workbook;
+import jxl.read.biff.BiffException;
 import jxl.write.Label;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
@@ -49,4 +53,56 @@ public class TestJxl {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	@Test
+	public void readExcel() {
+		//创建workbook
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File("d:/jxl_test.xls"));
+			//获取第一个sheet(工作表)
+			Sheet sheet = workbook.getSheet(0);
+			for(int i=0;i<sheet.getRows();i++) {
+				for(int j=0;j<sheet.getColumns();j++) {
+					//(列，行)
+					Cell cell = sheet.getCell(j, i);
+					System.out.print(cell.getContents()+"  ");
+				}
+				
+				System.out.println();
+			}
+		} catch (BiffException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
